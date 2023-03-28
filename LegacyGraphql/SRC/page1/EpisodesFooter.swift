a8d0fac4d9e6e251ad331fa06bcf736e44e169c9
sushant-toraskar <sushanttoraskar61@gmail.com>
2023-03-28 11:55:53 +0530

    WIP episodes page

---
+++ LegacyGraphql/SRC/page1/EpisodesFooter.swift
@@ -0,0 +1,17 @@
+//
+//  EpisodesFooter.swift
+//  LegacyGraphql
+//
+//  Created by Neosoft on 27/03/23.
+//
+
+import UIKit
+
+class EpisodesFooter: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
